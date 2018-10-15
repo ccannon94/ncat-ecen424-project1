5ffd71fbf7930bcb29aa18f9c67e9426cbf36464
Chris Cannon <Chris.c.cannon@gmail.com>
2018-10-15 16:10:58 -0400

    create empty output behavior

--- src/output_controller.vhd
+++ src/output_controller.vhd
@@ -4,3 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity output_controller is
   port(display : in std_logic_vector(3 downto 0); timeout, clk : in std_logic; enable, reset, led : out std_logic; seven_seg : std_logic_vector(11 downto 0));
 end entity output_controller;
+
+architecture output_behavior is
+
+end architecture output_behavior;
