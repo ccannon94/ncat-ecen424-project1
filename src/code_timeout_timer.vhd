b23d24fb01c4810ec1c6c0f1249e86d1febfbb85
Chris Cannon <Chris.c.cannon@gmail.com>
2018-10-24 10:20:39 -0400

    Create outline for code timeout timer

---
+++ src/code_timeout_timer.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity code_timeout_timer is
+  port(enable, reset, clk : in std_logic; done : out std_logic);
+end entity code_timeout_timer;
+
+architecture code_timeout_behavior is
+
+end architecture code_timeout_behavior;
