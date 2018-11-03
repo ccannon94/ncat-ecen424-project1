1f575bfd3e8f5e1bf278980a1c056d6684575fdc
Chris Cannon <chris.c.cannon@gmail.com>
2018-11-03 10:25:10 -0400

    Working code timeout timer

--- src/clock_divider.vhd
+++ src/clock_divider.vhd
@@ -5,10 +5,12 @@ use IEEE.NUMERIC_STD.ALL;
 entity clock_divider is
      port(clk : in std_logic;
           start_timer : in std_logic;
-	  input_clock,output_clock: out std_logic);
+	  FastClock,MediumClock,SlowClock, led0 : out std_logic);
 end clock_divider;
 
 architecture clock_divider_arch of clock_divider is
+signal slowClock_sig : STD_LOGIC;
+
 begin
     process
     variable cnt :	std_logic_vector(26 downto 0):= "000000000000000000000000000";
@@ -19,9 +21,15 @@ begin
 	    else
            cnt := STD_LOGIC_VECTOR(unsigned(cnt) + 1);
 	    end if;
-   	    output_clock <= cnt(22);
-   	    input_clock <= cnt(26);
+   	    FastClock <= cnt(22);
+   	    MediumClock <= cnt(24);
+   	    SlowClock <= cnt(26);
+        slowClock_sig <= cnt(26);
+        if (slowClock_sig = '1') then
+		  led0 <= '1';
+	    else
+		  led0 <= '0';
 	    end if;
-    end if;
 	end process;
+
 end clock_divider_arch;
--- src/code_timeout_timer.vhd
+++ src/code_timeout_timer.vhd
@@ -1,25 +1,31 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
 
 entity code_timeout_timer is
-  port(enable, reset, clk : in std_logic; done : out std_logic);
+  port(enable, reset, one_hz_clk : in std_logic; done : out std_logic);
 end entity code_timeout_timer;
 
 architecture code_timeout_behavior of code_timeout_timer is
 
-signal time : integer range 0 to 20 := 0;
+signal current_count : std_logic_vector(4 downto 0) := "00000";
+
 
 begin
-  process(clk, reset)
-  begin
-    if(reset = '1') then
-      time := 0;
-      done <= '0';
-    elsif(clk'event and clk = '1' and enable = '1') then
-      time := time + 1;
-      if(time >= 1000000000) then
-        done <= '1';
-      end if;
-    end if;
-  end process;
+
+    process(one_hz_clk, reset)
+    begin
+        if(reset = '1') then 
+	    current_count <= "00000";
+	    done <= '0';
+        elsif(one_hz_clk'event and one_hz_clk = '1' and enable = '1') then
+            if(current_count <= "10100") then
+                current_count <= std_logic_vector(unsigned(current_count) + 1);
+                done <= '0';
+            else
+                done <= '1';
+            end if;
+        end if;
+end process;
+
 end architecture code_timeout_behavior;
