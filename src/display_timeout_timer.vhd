56ea421d7189c7bf9e8fe6000a3e0e6af90db4b4
Chris Cannon <chris.c.cannon@gmail.com>
2018-11-03 12:33:50 -0400

    Update display timer with new algorithm

--- src/display_timeout_timer.vhd
+++ src/display_timeout_timer.vhd
@@ -2,24 +2,29 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity display_timeout_timer is
-  port(enable, reset, clk : in std_logic; done : out std_logic);
+  port(enable, reset, one_hz_clk : in std_logic; done : out std_logic);
 end entity display_timeout_timer;
 
 architecture display_timeout_behavior of display_timeout_timer is
 
-signal time : integer range 0 to 20 := 0;
+signal current_count : std_logic_vector(4 downto 0) := "00000";
+
 
 begin
-  process(clk, reset)
-  begin
-    if(reset = '1') then
-      time := 0;
-      done <= '0';
-    elsif(clk'event and clk = '1' and enable = '1') then
-      time := time + 1;
-      if(time >= 25000000) then
-        done <= '1';
-      end if;
-    end if;
-  end process;
+
+    process(one_hz_clk, reset)
+    begin
+        if(reset = '1') then
+	    current_count <= "00000";
+	    done <= '0';
+        elsif(one_hz_clk'event and one_hz_clk = '1' and enable = '1') then
+            if(current_count <= "00101") then
+                current_count <= std_logic_vector(unsigned(current_count) + 1);
+                done <= '0';
+            else
+                done <= '1';
+            end if;
+        end if;
+end process;
+
 end architecture display_timeout_behavior;
